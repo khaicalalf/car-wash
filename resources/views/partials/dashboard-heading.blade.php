b50abaf5bb71a036c875a11b6d19d69bb1117ad5
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2025-09-18 15:31:38 +0700

    first commit

---
+++ resources/views/partials/dashboard-heading.blade.php
@@ -0,0 +1,5 @@
+<div class="relative mb-6 w-full">
+    <flux:heading size="xl" level="1">{{ __('Welcome') }}</flux:heading>
+    <flux:subheading size="lg" class="mb-6">{{ __('Manage your booking in here') }}</flux:subheading>
+    <flux:separator variant="subtle" />
+</div>
